e68bcf9d5005ea0f377bdb9e87f99c112fa77cc0
Cons Sh <umreen@gmail.com>
2022-05-04 17:19:24 +0300

    prep for npc

---
+++ npc.py
@@ -0,0 +1,54 @@
+def full_name():
+    pass
+
+
+def race():
+    pass
+
+
+def hp():
+    pass
+
+
+def str():
+    pass
+
+
+def dex():
+    pass
+
+
+def cha():
+    pass
+
+
+def armour():
+    pass
+
+
+def weapon():
+    pass
+
+
+def traits():
+    pass
+
+
+def profession():
+    pass
+
+
+def wants():
+    pass
+
+
+def secret():
+    pass
+
+
+def looks():
+    pass
+
+
+def activity():
+    pass
