e580d90715843c038c7bc8fa333679bfdee89b58
Cons Sh <umreen@gmail.com>
2022-05-04 17:16:49 +0300

    type hints

--- dice.py
+++ dice.py
@@ -1,33 +1,33 @@
 from random import randint
 
 
-def d3():
+def d3() -> int:
     return randint(1, 3)
 
 
-def d4():
+def d4() -> int:
     return randint(1, 4)
 
 
-def d6():
+def d6() -> int:
     return randint(1, 6)
 
 
-def d8():
+def d8() -> int:
     return randint(1, 8)
 
 
-def d10():
+def d10() -> int:
     return randint(1, 10)
 
 
-def d12():
+def d12() -> int:
     return randint(1, 12)
 
 
-def d20():
+def d20() -> int:
     return randint(1, 20)
 
 
-def d100():
+def d100() -> int:
     return randint(1, 100)
