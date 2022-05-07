2d954777c5704c442c2f4919a8dde9f9e9aa8e8b
Cons Sh <umreen@gmail.com>
2022-05-07 09:51:08 +0300

    tinkering with stuff

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-
+import json
 
 from dice import *
 
 
-print(d3())
+def load_careers(path):
+    file = open(path, mode='r')
+
+    # read all lines at once
+    all_of_it = file.read()
+
+    js = json.loads(all_of_it)
+    print("Profession Title: ", js[0]['professionTitle'])
+
+
+load_careers('data/careers.json')
